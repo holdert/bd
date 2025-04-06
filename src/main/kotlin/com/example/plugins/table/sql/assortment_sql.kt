39e9bfcf5cd8136fe758ef9bf65154739d5a76aa
Michael <147973061+Limkor@users.noreply.github.com>
2025-04-06 16:08:09 +0300

    Update assortment_sql.kt
    Fixed an error connecting to the database

--- src/main/kotlin/com/example/plugins/table/sql/assortment_sql.kt
+++ src/main/kotlin/com/example/plugins/table/sql/assortment_sql.kt
@@ -7,25 +7,44 @@ import com.example.plugins.table.sql.MySQLDatabaseExampleKotlin.stmt
 import java.sql.SQLException
 
 fun update_assortment(assortId: bd_id, assort: assortment, tableName: String) {
-    try {
-        val num: Int = assortId.bd_id.toInt()
-        stmt = conn!!.createStatement()
-        if (assort.name != "" && assort.description != "") {
-            val sql =
-                """UPDATE $tableName SET name = '${assort.name}', description = '${assort.description}' WHERE id_$tableName = '${num}';"""
-            stmt!!.executeUpdate(sql)
-        }else if (assort.name != ""){
-            val sql = """UPDATE $tableName SET name = '${assort.name}' WHERE id_$tableName = '${num}';"""
-            stmt!!.executeUpdate(sql)
-            //println("dddddddddddddddddddddddddddddddddd")
-        }else if(assort.description != ""){
-            val sql = """UPDATE $tableName SET description = '${assort.description}' WHERE id_$tableName = '${num}';"""
-            stmt!!.executeUpdate(sql)
+try {
+    val num: Int = assortId.bd_id.toInt()
+    if (assort.name != "" || assort.description != "") {
+        val sql = when {
+            assort.name != "" && assort.description != "" -> 
+                "UPDATE $tableName SET name = ?, description = ? WHERE id_$tableName = ?"
+            assort.name != "" -> 
+                "UPDATE $tableName SET name = ? WHERE id_$tableName = ?"
+            else -> 
+                "UPDATE $tableName SET description = ? WHERE id_$tableName = ?"
         }
-    }catch (ex: SQLException){
-        ex.printStackTrace()
+        
+        val preparedStmt = conn!!.prepareStatement(sql)
+        
+        when {
+            assort.name != "" && assort.description != "" -> {
+                preparedStmt.setString(1, assort.name)
+                preparedStmt.setString(2, assort.description)
+                preparedStmt.setInt(3, num)
+            }
+            assort.name != "" -> {
+                preparedStmt.setString(1, assort.name)
+                preparedStmt.setInt(2, num)
+            }
+            else -> {
+                preparedStmt.setString(1, assort.description)
+                preparedStmt.setInt(2, num)
+            }
+        }
+        
+        preparedStmt.executeUpdate()
     }
+} catch (e: Exception) {
+    // Обработка ошибок
+} finally {
+    stmt?.close()
 }
+
 fun into_assortment(assort: assortment, tableName: String) {
     try {
         stmt = conn!!.createStatement()
